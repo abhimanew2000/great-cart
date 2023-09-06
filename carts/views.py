e8e9a52c85b49153fba7b038f71f7d652be299bc
abhimanyu <abhimanew2000@gmail.com>
2023-09-06 11:32:34 +0530

    callbackurl changetry

--- carts/views.py
+++ carts/views.py
@@ -572,7 +572,7 @@ def place_order(request):
                 request,
                 "store/payment.html",
                 {
-                "callback_url": "http://" + "www.abhimanew.live" + "/carts/callback/?current_order={}".format(current_order),
+                "callback_url": "http://" + "abhimanew.live" + "/carts/callback/?current_order={}".format(current_order),
                     "razorpay_key": RAZORPAY_KEY_ID,
                     "orders": orders,
                     "final_total": final_total,
